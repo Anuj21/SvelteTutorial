a2af6b12933236e045756f0cf424e4df16b94ced
AJ2128 <anujjoshi21@gmail.com>
2022-03-19 13:22:35 +0100

    Initial Commit

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,132 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import ist from './git-icon.svg';
+	import { DateTime } from 'luxon/src/luxon';
+
+	let ter: string = "";
+	setInterval(() => {
+		let dateTime: DateTime = DateTime.local();
+		ter = dateTime.toLocaleString(DateTime.DATETIME_SHORT);
+	}, 1);
+</script>
+
+<header>
+	<div class="corner">
+		<a href="https://github.com/Anuj21" target="_blank">
+			<img src={ist} alt="Github Account" />
+		</a>
+	</div>
+
+	<nav>
+		<ul>
+			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+			<li class:active={$page.url.pathname === '/about'}>
+				<a sveltekit:prefetch href="/about">About</a>
+			</li>
+			<li class:active={$page.url.pathname === '/todos'}> 
+				<a sveltekit:prefetch href="/todos">Todos</a>
+			</li>
+		</ul>
+	</nav>
+
+	<div class="corner right">
+		{ter}
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		background: rgba(255, 255, 255, 0.7);
+	}
+
+	.corner {
+		width: 3em;
+		height: 3em;
+	}
+
+	
+	.corner.right {
+		display: flex;
+		align-items: center;
+		width: 6em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner img {
+		width: 2em;
+		height: 2em;
+		object-fit: contain;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+	}
+
+	svg {
+		width: 2em;
+		height: 3em;
+		display: block;
+	}
+
+	path {
+		fill: var(--background);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li.active::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--accent-color);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 1em;
+		color: var(--heading-color);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--accent-color);
+	}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>visit <a href="https://github.com/Anuj21" target="_blank">My Github Account</a> to see more projects.</p>
+</footer>
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+
+		padding: 1rem;
+		
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+ 	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+</style>
